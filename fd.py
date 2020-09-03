966e7e4f9f6c8e710b6e23ec64959d7ec861dfc3
lazarussmith123 <68512633+lazarussmith123@users.noreply.github.com>
2020-09-03 01:29:31 +0100

    Add files via upload

---
+++ fd.py
@@ -0,0 +1,12 @@
+import datetime
+today = datetime.datetime.now()
+str(today)
+repr(today)
+ 
+ class Represent(object):
+     
+     def __init__(self, x, y)
+         self .x,  self.y = x,y
+         
+     def  __repr
+                  
